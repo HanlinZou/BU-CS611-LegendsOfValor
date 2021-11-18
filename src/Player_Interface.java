da98f06a110168f7dcd4cd7a190a0f0dcb0c5ebf
Hanlin Zou <haz272@ucsd.edu>
2021-11-18 21:15:53 +0800

    修改typo 上传player interface

---
+++ src/Player_Interface.java
@@ -0,0 +1,20 @@
+/**
+ * Player Interface is designed to satisfy potential scalability and extendability
+ * purpose. Convenient to build features such as playerId, teams, etc.
+ */
+
+public interface Player_Interface {
+
+    void setPlayerName(String name);
+
+    void setTeam(int id, String s);
+
+    String getPlayerName();
+
+    String getTeamName();
+
+    int getTeamId();
+
+    String toString();
+
+}
